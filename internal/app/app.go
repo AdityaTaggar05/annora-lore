5c1720cca2d11561007d1901dfaaf25dbcce284d
AdityaTaggar05 <aditya05.mgg@gmail.com>
2026-01-03 01:09:38 +0530

    Added the setup of a basic go-chi server and router

---
+++ internal/app/app.go
@@ -0,0 +1,60 @@
+package app
+
+import (
+	"context"
+	"log"
+	"net/http"
+	"os"
+	"os/signal"
+	"syscall"
+	"time"
+
+	"github.com/AdityaTaggar05/annora-lore/internal/config"
+	"github.com/AdityaTaggar05/annora-lore/internal/handlers/https"
+)
+
+type App struct {
+	Config *config.Config
+	Server *http.Server
+}
+
+func New(cfg *config.Config) (*App, error) {
+	router := https.NewRouter()
+
+	return &App{
+		Config: cfg,
+		Server: &http.Server{
+			Addr:         ":" + cfg.Server.Port,
+			Handler: router,
+			ReadTimeout: cfg.Server.ReadTimeout,
+			WriteTimeout: cfg.Server.WriteTimeout,
+		},
+	}, nil
+}
+
+func (a *App) Start() error {
+	stop := make(chan os.Signal, 1)
+	signal.Notify(stop, os.Interrupt, syscall.SIGTERM)
+
+	go func() {
+		log.Printf("Lore service listening on %s\n", a.Server.Addr)
+
+		if err := a.Server.ListenAndServe(); err != nil && err != http.ErrServerClosed {
+			log.Fatalf("HTTP server error: %v\n", err)
+		}
+	}()
+
+	<-stop
+	log.Println("Shutdown signal received")
+
+	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+	defer cancel()
+
+	if err := a.Server.Shutdown(ctx); err != nil {
+		log.Printf("Server shutdown failed: %v", err)
+		return err
+	}
+
+	log.Println("Auth service stopped gracefully")
+	return nil
+}
---
+++ internal/handlers/https/router.go
@@ -0,0 +1,18 @@
+package https
+
+import (
+	"net/http"
+
+	"github.com/go-chi/chi"
+)
+
+func NewRouter() *chi.Mux {
+	r := chi.NewRouter()
+
+	r.Get("/", func(w http.ResponseWriter, r *http.Request) {
+		w.Write([]byte("Service is up and running!"))
+		w.WriteHeader(http.StatusOK)
+	})
+
+	return r
+}
