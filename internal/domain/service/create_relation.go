ad87c203c768d07335b00e33cf647f6ed5ce0e7f
AdityaTaggar05 <aditya05.mgg@gmail.com>
2026-03-17 00:00:06 +0530

    [fix] update relation creation constraints
    Fixes #2

--- internal/domain/service/create_relation.go
+++ internal/domain/service/create_relation.go
@@ -2,6 +2,7 @@ package service
 
 import (
 	"context"
+	"errors"
 
 	"github.com/AdityaTaggar05/annora-lore/internal/api/https/dto/request"
 	"github.com/AdityaTaggar05/annora-lore/internal/domain/model"
@@ -10,6 +11,10 @@ import (
 
 func (s *LoreService) CreateRelation(ctx context.Context, dto request.CreateRelationRequestDTO) (*model.LoreNode, *model.Relation, *model.LoreNode, error) {
 	worldID, relation := dto.ToDomain()
+
+	if dto.FromID == dto.ToID {
+		return nil, nil, nil, errors.New("cannot create a self-pointing relation")
+	}
 
 	err := validation.ValidateRelation(model.NodeType(dto.FromType), model.NodeType(dto.ToType), model.RelationType(dto.Relation))
 	if err != nil {
